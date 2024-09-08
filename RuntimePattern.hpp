9bbaa4c95e1ffe52ec123648e081c1b0ba01bd2c
scizzydo <34661795+scizzydo@users.noreply.github.com>
2024-09-08 16:10:30 -0700

    Skip assigning size_ on arm as it doesn't exist

--- RuntimePattern.hpp
+++ RuntimePattern.hpp
@@ -23,7 +23,9 @@ namespace patterns {
                 else if (*ptr == 'X' || *ptr == 'x') {
                     offset_ = n;
                     if (p[i + 1] != ' ') {
+#ifndef __arm64__
                         size_ = get_inst_len_opt(&p[++i]);
+#endif
                         while (p[i + 1] != ' ')
                             ++i;
                     }
